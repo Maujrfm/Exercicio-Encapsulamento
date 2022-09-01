2a7bf4ebeae898a9c07c7f405703b69451407516
Mauricio Junior <mjrfmoraes@outlook.com>
2022-09-01 12:41:08 -0400

    Exercicio Fixação Encapsulamento

---
+++ Exercico Encapsulamento/Exercico Encapsulamento/Conta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercico_Encapsulamento
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public String Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numeroconta, string titular)
+        {
+            NumeroConta = numeroconta;
+            Titular = titular;
+        }
+
+        public Conta(int numeroconta, string titular, double saldo) : this(numeroconta, titular)
+        {
+            DepositoConta(saldo);
+        }
+
+
+        public void DepositoConta(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void SaqueConta(double valor)
+        {
+            Saldo -= valor + 5.00;
+
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta.ToString("D4") + ", Titular: "
+                + Titular + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercico Encapsulamento/Exercico Encapsulamento/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+namespace Exercico_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+
+            Console.WriteLine("Entre com o Numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o Titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Haverá deposito incial? (s/n)");
+            char depi = char.Parse(Console.ReadLine());
+            if (depi == 's' || depi == 'S')
+            {
+                Console.WriteLine("Entre o valor de deposito inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(conta, titular, saldo);
+            }
+            else
+            {
+                double saldo = 0.00;
+                c = new Conta(conta, titular, saldo);
+            }
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(c);
+            Console.WriteLine();
+            Console.WriteLine("Entre com um valor para deposito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.DepositoConta(valor);
+            Console.WriteLine("Dados Atualizados: ");
+            Console.WriteLine(c);
+            Console.WriteLine("Entre com um valor para Saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.SaqueConta(valor);
+            Console.WriteLine("Dados Atualizados: ");
+            Console.WriteLine(c);
+        }
+    }
+}
